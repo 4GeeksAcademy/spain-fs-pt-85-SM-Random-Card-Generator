7ccc70a5c2f4695b9f65d4e4ec9fdb6d4949787d
unduashe <unduashe@outlook.es>
2024-11-12 17:55:51 +0000

    optimizado código para la comprobación del color de las cartas y que permita generar solo una mano de poker

--- script.js
+++ script.js
@@ -14,13 +14,6 @@ function cardDeck(){
     return randomCardDeck;
 }
 
-// revisar esta función
-function cardDeckColorCheker(deck){
-    let newRandomDeck = cardDeck()
-    if (newRandomDeck == "♥" || newRandomDeck == "♦") {
-        deck.parentNode.attributes.class.nodeValue += " text-danger"
-        }
-}
 
 function cardNumber(){
     let englishDeckNumbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
@@ -36,30 +29,28 @@ function refreshPage(){
     });
 }
 
-// revisar la lógica del if para ver si se puede eliminar
+
 function generateFirstCard(){
     let firstDiv = document.querySelector(".row");
     let newDeck = cardDeck();
     let newNumber = cardNumber();
-    if (newDeck == "♥" || newDeck == "♦") {
-        firstDiv.innerHTML = `<div class="card shadow-lg col-lg-5 col-md-5 col-sm-11  m-4">
-            <div class="deck fs-0 position-absolute top-0 start-0 px-2 text-danger">${newDeck}</div>
-            <div id="card-number" class="fs-0 text-center text-black position-absolute top-50 start-50 translate-middle">${newNumber}</div>
-            <div class="deck inverted-view fs-0 text-end position-absolute bottom-0 end-0 px-2 text-danger">${newDeck}</div>
-        </div>`
-    }
-    else {
-        firstDiv.innerHTML = `<div class="card shadow-lg col-lg-5 col-md-5 col-sm-11  m-4">
-            <div class="deck fs-0 position-absolute top-0 start-0 px-2">${newDeck}</div>
-            <div id="card-number" class="fs-0 text-center text-black position-absolute top-50 start-50 translate-middle">${newNumber}</div>
-            <div class="deck inverted-view fs-0 text-end position-absolute bottom-0 end-0 px-2">${newDeck}</div>
-        </div>`
-    }
-
+    firstDiv.innerHTML = `<div class="card shadow-lg col-lg-5 col-md-5 col-sm-11  m-4">
+                            <div class="deck fs-0 position-absolute top-0 start-0 px-2">${newDeck}</div>
+                            <div id="card-number" class="fs-0 text-center text-black position-absolute top-50 start-50 translate-middle">${newNumber}</div>
+                            <div class="deck inverted-view fs-0 text-end position-absolute bottom-0 end-0 px-2">${newDeck}</div>
+                          </div>`
+    let desckElements = firstDiv.querySelectorAll(".deck");
+    desckElements.forEach(deckElement => cardDeckColorCheker(deckElement,newDeck));
 }
 
 
-// revisar la creación de nuevas cartas para que le añada color al palo
+function cardDeckColorCheker(deckElement, newDeck){
+    if (newDeck == "♥" || newDeck == "♦") {
+        deckElement.classList.add("text-danger");
+        }
+}
+
+
 function generateNewCard(){
     let newDeck = cardDeck();
     let newNumber = cardNumber();
@@ -67,39 +58,19 @@ function generateNewCard(){
     let newDiv = document.createElement("div");
     newDiv.className = "card shadow-lg col-lg-5 col-md-5 col-sm-11  m-4";
     newDiv.innerHTML = `<div class="deck fs-0 position-absolute top-0 start-0 px-2">${newDeck}</div>
-            <div id="card-number" class="fs-0 text-center text-black position-absolute top-50 start-50 translate-middle">${newNumber}</div>
-            <div class="deck inverted-view fs-0 text-end position-absolute bottom-0 end-0 px-2">${newDeck}</div>`
+                        <div id="card-number" class="fs-0 text-center text-black position-absolute top-50 start-50 translate-middle">${newNumber}</div>
+                        <div class="deck inverted-view fs-0 text-end position-absolute bottom-0 end-0 px-2">${newDeck}</div>`
     firstDiv.appendChild(newDiv);
+    let deckElements = newDiv.querySelectorAll(".deck")
+    deckElements.forEach(deckElement => cardDeckColorCheker(deckElement, newDeck))
 }
 
 
 function newCardButtonListener(){
     let newCardListener = document.querySelector("#new-card");
+    let cardCounter = 1;
     newCardListener.addEventListener("click", (e) => {
+        cardCounter < 4 ? cardCounter++ : newCardListener.parentNode.removeChild(newCardListener);
         generateNewCard();
     });
 }
-
-
-
-
-// function generateNewCard(){
-//     let newDeck = cardDeck();
-//     let newNumber = cardNumber();
-//     let firstDiv = document.querySelector(".row");
-//     let newDiv = document.createElement("div");
-//     newDiv.className = "card shadow-lg col-lg-5 col-md-5 col-sm-11  mx-4";
-//     firstDiv.appendChild(newDiv);
-//     let newDeckDiv = document.createElement("div");
-//     newDeckDiv.innerHTML = `${newDeck}`;
-//     newDeckDiv.className = "deck fs-0 position-absolute top-0 start-0 px-2";
-//     newDiv.appendChild(newDeckDiv);
-//     let newCardNumberDiv = document.createElement("div");
-//     newCardNumberDiv.className = "fs-0 text-center text-black position-absolute top-50 start-50 translate-middle";
-//     newCardNumberDiv.innerHTML = newNumber;
-//     newDiv.appendChild(newCardNumberDiv);
-//     let secondNewDeckDiv = document.createElement("div");
-//     secondNewDeckDiv.className = "deck inverted-view fs-0 text-end position-absolute bottom-0 end-0 px-2";
-//     secondNewDeckDiv.innerHTML = `${newDeck}`;
-//     newDiv.appendChild(secondNewDeckDiv);
-// }
